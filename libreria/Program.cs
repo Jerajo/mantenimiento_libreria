7ad9095d0092d1871a72538bf6de51bec45f0924
Jerajo <jessejose@outlook.es>
2017-11-28 20:55:18 -0300

    Que empieze con FrmLibros

--- libreria/Program.cs
+++ libreria/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using libreria.forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,9 @@ namespace libreria
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            //Application.Run(FrmLogin.GetInstance());
+            Application.Run(new FrmLibros());
         }
     }
 }
