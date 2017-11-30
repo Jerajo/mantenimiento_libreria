850a5e9cd3600c7da668d4518e1e9c45f1554d46
Jerajo <jessejose@outlook.es>
2017-11-30 02:20:04 -0300

    Sistema de Ctegorias agregado

---
+++ libreria/conexión/CCategoria.cs
@@ -0,0 +1,58 @@
+﻿using Sistema_de_punto_de_ventas.Entidades;
+using SisVenttas.Datos;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Sistema_de_punto_de_ventas.Datos
+{
+    public class CCategoria
+    {
+        public static DataSet GetAll()
+        {
+            SqlParameter[] dbParams = new SqlParameter[]
+                {
+
+                };
+            return FDBHelper.ExecuteDataSet("usp_Data_CCategoria_GetAll", dbParams);
+        }
+
+
+        public static int Insertar(Libro libro)
+        {
+            SqlParameter[] dbParams = new SqlParameter[]
+                {
+                    //, Titulo, Pais, Stock, Editorial, CategoriaId
+                    FDBHelper.MakeParam("@ISBN",SqlDbType.VarChar, 0, libro.ISBN),
+                    FDBHelper.MakeParam("@Titulo",SqlDbType.VarChar, 0, libro.Titulo),
+                    FDBHelper.MakeParam("@Pais",SqlDbType.VarChar, 0, libro.Pais),
+                    FDBHelper.MakeParam("@Stock",SqlDbType.Int, 0, libro.Stock),
+                    FDBHelper.MakeParam("@Editorial",SqlDbType.VarChar, 0, libro.Editorial),
+                    FDBHelper.MakeParam("@CategoriaId",SqlDbType.Int, 0, libro.CategoriaId),                
+                };
+            return Convert.ToInt32(FDBHelper.ExecuteScalar("usp_Data_CCategoria_Insertar", dbParams));
+        }
+
+        public static int Actualizar(Libro libro)
+        {
+            SqlParameter[] dbParams = new SqlParameter[]
+                {
+                    FDBHelper.MakeParam("@ISBN",SqlDbType.VarChar, 0, libro.ISBN),
+                    FDBHelper.MakeParam("@Pais",SqlDbType.VarChar, 0, libro.Pais),
+                    FDBHelper.MakeParam("@Stock",SqlDbType.Int, 0, libro.Stock),
+                    FDBHelper.MakeParam("@Editorial",SqlDbType.VarChar, 0, libro.Editorial),
+                    FDBHelper.MakeParam("@CategoriaId",SqlDbType.Int, 0, libro.CategoriaId),
+                };
+            return Convert.ToInt32(FDBHelper.ExecuteScalar("usp_Data_CCategoria_Actualizar", dbParams));
+        }
+
+        public static int Eliminar(Libro libro)
+        {
+            SqlParameter[] dbParams = new SqlParameter[]
+                {
+                    FDBHelper.MakeParam("@ISBN",SqlDbType.VarChar, 0, libro.ISBN),
+                };
+            return Convert.ToInt32(FDBHelper.ExecuteScalar("usp_Data_CCategoria_Eliminar", dbParams));
+        }
+    } 
+}
---
+++ libreria/entidades/Categoria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_punto_de_ventas.Entidades
+{
+    public class Categoria
+    {
+        private int _id;
+        private string _genero;
+        public Categoria() {}
+        public Categoria(int id, string genero)
+        {
+            this._id = id;
+            this._genero = genero;
+        }
+        public int Id { get => _id; set => _id = value; }
+        public string Genero { get => _genero; set => _genero = value; }        
+    }
+}
