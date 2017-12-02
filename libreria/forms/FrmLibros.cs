dec5200ca86886a2ca298f8c8f9733fa50b4faeb
Jesus Francisco Dicent Aguero <jesusdicent@gmail.com>
2017-12-02 18:51:45 -0400

    Pequeños cambios sin importancia

--- libreria/forms/FrmLibros.cs
+++ libreria/forms/FrmLibros.cs
@@ -324,5 +324,10 @@ namespace libreria.forms
             var regexItem = new Regex("^[a-zA-Z0-9 ()-_.,#]*$");
             if (!regexItem.IsMatch(Convert.ToString(e.KeyChar)) && !(e.KeyChar == '\b')) e.Handled = true;
         }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
     }
 }
