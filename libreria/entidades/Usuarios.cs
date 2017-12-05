4825c0c90582b0b9478c9d6c5fde6dbe6dffec39
Jerajo <jessejose@outlook.es>
2017-12-05 09:39:38 -0300

    clase usuarios agregada

---
+++ libreria/entidades/Usuarios.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libreria.entidades
+{
+    public static class Usuarios
+    {
+        private static string _username;
+        private static int _password;
+
+        public static string Username { get => _username; set => _username = value; }
+        public static int Password { get => _password; }
+        public static void SetPassword(string value)
+        {
+
+        }
+    }
+}
