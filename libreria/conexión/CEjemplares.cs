2b041c6930676a154822d0e348823a926a967ca1
Jerajo <jessejose@outlook.es>
2017-12-05 18:10:17 -0300

    CEjemplages agregado

---
+++ libreria/conexión/CEjemplares.cs
@@ -0,0 +1,69 @@
+﻿using libreria.entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libreria.conexión
+{
+    class CEjemplares
+    {
+        public static DataSet GetAll()
+        {
+            SqlParameter[] dbParams = new SqlParameter[] { };
+            return DatabaseCon.ExecuteDataSet("usp_Data_CEjemplares_GetAll", dbParams);
+        }
+
+        public static DataSet GetColumnNames()
+        {
+            SqlParameter[] dbParams = new SqlParameter[]
+            {
+                DatabaseCon.MakeParam("@tabla",SqlDbType.VarChar, "[dbo].[EjemplaressSet]")
+            };
+            return DatabaseCon.ExecuteDataSet("usp_Data_CCategoria_GetColumnNames", dbParams);
+        }
+
+        public static int Insertar(Ejemplares ejemplares)
+        {
+            SqlParameter[] dbParams = new SqlParameter[]
+                {
+                    DatabaseCon.MakeParam("@ISBN",SqlDbType.VarChar, ejemplares.ISBN),
+                    DatabaseCon.MakeParam("@Cod",SqlDbType.VarChar, ejemplares.Codigo),
+                    DatabaseCon.MakeParam("@numero",SqlDbType.Int, ejemplares.Numero)
+                };
+            return Convert.ToInt32(DatabaseCon.ExecuteScalar("usp_Data_Ejemplares_Insertar", dbParams));
+        }
+
+        public static int Actualizar(Ejemplares ejemplares)
+        {
+            SqlParameter[] dbParams = new SqlParameter[]
+                {
+                    DatabaseCon.MakeParam("@ISBN",SqlDbType.VarChar, ejemplares.ISBN),
+                    DatabaseCon.MakeParam("@Cod",SqlDbType.VarChar, ejemplares.Codigo),
+                    DatabaseCon.MakeParam("@numero",SqlDbType.Int, ejemplares.Numero)
+                };
+            return Convert.ToInt32(DatabaseCon.ExecuteScalar("usp_Data_Ejemplares_Actualizar", dbParams));
+        }
+
+        public static int Eliminar(Ejemplares ejemplares)
+        {
+            SqlParameter[] dbParams = new SqlParameter[]
+                {
+                    DatabaseCon.MakeParam("@ISBN",SqlDbType.VarChar, ejemplares.ISBN),
+                };
+            return Convert.ToInt32(DatabaseCon.ExecuteScalar("usp_Data_CEjemplares_Eliminar", dbParams));
+        }
+
+        public static int VerificarStock(int dni)
+        {
+            SqlParameter[] dbParams = new SqlParameter[]
+                {
+                    DatabaseCon.MakeParam("@Stock",SqlDbType.Int, dni),
+                };
+            return Convert.ToInt32(DatabaseCon.ExecuteScalar("usp_Data_CEjemplares_VerificarStock", dbParams));
+        }
+    }
+}
